dc6f2479097c2b173a9de5e2286b27af921c037b
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-01-22 18:38:42 -0800

    lein with exclusions?

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [midje "1.6.0"]]
-  :plugins [[lein-midje "3.1.3"]])
+  :plugins [[lein-midje "3.1.3"] :exclusions [org.clojure/clojure]])
