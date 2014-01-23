fa8061a4eb7efa5238da8030600a623de8d889fb
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-01-22 17:59:56 -0800

    updating lein plugin

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.6.0"]])
+                 [midje "1.6.0"]]
+  :plugins [[lein-mide "3.1.3"]])
